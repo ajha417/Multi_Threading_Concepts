7e24fdc8ee2f97e63459b66074b16a3e112e4897
ajha417 <ajha417@rku.ac.in>
2024-12-31 22:49:07 +0530

    adds thread communication example

---
+++ com/learn/ThreadCommunication.java
@@ -0,0 +1,16 @@
+package com.learn;
+
+class SharedResource {
+    public void consume() {
+
+    }
+
+    public void produce() {
+
+    }
+}
+public class ThreadCommunication {
+    public static void main(String[] args) {
+
+    }
+}
