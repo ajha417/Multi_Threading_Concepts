7a4666d1e074cf8fa738f44a3bea2779a78b9444
ajha417 <ajha417@rku.ac.in>
2025-01-07 22:06:48 +0530

    adds shared resource class

--- com/learn/ThreadCommunication.java
+++ com/learn/ThreadCommunication.java
@@ -1,12 +1,37 @@
 package com.learn;
 
 class SharedResource {
+
+    private int data;
+    private boolean hasData;
     public void consume() {
 
     }
 
-    public void produce() {
+    public void produce(int data) {
 
+    }
+}
+
+class Producer implements Runnable {
+
+    private SharedResource resource;
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            resource.produce(i);
+            System.out.println("produced: " + i);
+        }
+    }
+}
+class Consumer implements Runnable {
+
+    private SharedResource resource;
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+
+        }
     }
 }
 public class ThreadCommunication {
