1bdde4c7d777308a184c5692092cda3194bd9ad3
ajha417 <ajha417@rku.ac.in>
2025-01-08 21:44:19 +0530

    thread communication example

--- com/learn/ThreadCommunication.java
+++ com/learn/ThreadCommunication.java
@@ -4,38 +4,69 @@ class SharedResource {
 
     private int data;
     private boolean hasData;
-    public void consume() {
-
+    public synchronized int consume() {
+        while (!hasData) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        hasData = false;
+        System.out.println("Consumed:" + data);
+        notify();
+        return data;
     }
 
-    public void produce(int data) {
-
+    public synchronized void produce(int value) {
+        while (hasData) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        data = value;
+        System.out.println("produced: " + value);
+        hasData = true;
+        notify();
     }
 }
 
 class Producer implements Runnable {
 
     private SharedResource resource;
+    public Producer(SharedResource resource) {
+        this.resource = resource;
+    }
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             resource.produce(i);
-            System.out.println("produced: " + i);
         }
     }
 }
 class Consumer implements Runnable {
 
     private SharedResource resource;
+
+    public Consumer(SharedResource resource) {
+        this.resource = resource;
+    }
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-
+            resource.consume();
         }
     }
 }
 public class ThreadCommunication {
     public static void main(String[] args) {
+        SharedResource resource = new SharedResource();
+        Thread producerThread = new Thread(new Producer(resource));
+        Thread consumerThread = new Thread(new Consumer(resource));
 
+        producerThread.start();
+        consumerThread.start();
     }
 }
