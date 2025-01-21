65f969a91af9d9a60e1016bb2fb5b9b00a5a5f7d
ajha417 <ajha417@rku.ac.in>
2025-01-21 22:47:31 +0530

    adds invokeall example

--- com/learn/ExecutorFramework/Main.java
+++ com/learn/ExecutorFramework/Main.java
@@ -1,9 +1,11 @@
 package com.learn.ExecutorFramework;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args)  {
         long startTime = System.currentTimeMillis();
 //        Thread[] threads = new Thread[9];
 //        for (int i = 1; i < 10; i++) {
@@ -45,14 +47,66 @@ public class Main {
 //        }
 
 //        submit method inside return Future which means we can do certain task based on that
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Future<?> future = executorService.submit(()->"hello");
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        Future<?> future = executorService.submit(()->"hello");
+//
+//        System.out.println(future.get());
+//        if (future.isDone()) {
+//            System.out.println("Completed task");
+//        }
+//        executorService.shutdown();
 
-        System.out.println(future.get());
-        if (future.isDone()) {
-            System.out.println("Completed task");
+
+        // taking the example of invoke all method
+
+        Callable<Integer> callable1 = () -> {
+            Thread.sleep(1000);
+            System.out.println("task1");
+            return 1;
+        };
+
+        Callable<Integer> callable2 = () -> {
+            Thread.sleep(1000);
+            System.out.println("task2");
+            return 2;
+        };
+
+        Callable<Integer> callable3 = () -> {
+            Thread.sleep(1000);
+            System.out.println("task3");
+            return 3;
+        };
+
+        List<Callable<Integer>> taskList = Arrays.asList(callable1, callable2, callable3);
+
+        ExecutorService service = Executors.newFixedThreadPool(2);
+//        List<Future<Integer>> futures  = service.invokeAll(taskList);
+//        for (Future<Integer> future: futures) {
+//            future.get();
+//        }
+
+        // invokeAll() method has 2 version. first it takes collections of tasks. second it takes collection of tasks as well as time and unit
+        List<Future<Integer>> futureList = null;
+        try {
+            futureList = service.invokeAll(taskList, 1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+
         }
-        executorService.shutdown();
+
+        assert futureList != null;
+        for (Future<Integer> future: futureList) {
+            try {
+                System.out.println(future.get());
+            } catch (CancellationException e) {
+
+            }catch (InterruptedException e) {
+
+            } catch (ExecutionException e) {
+
+            }
+        }
+
+        service.shutdown();
         System.out.println("time taken:" + (System.currentTimeMillis() - startTime));
     }
 
